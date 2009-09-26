4789a78af7604f36a0722d830f334a8fdbc57449
nanakoso <nanakoso@gmail.com>
2009-09-27 03:36:54 +0900

    add first commit source Generator for Java Generator for Java 最初のJavaソース登録

---
+++ Generator/src/jp/gr/java_conf/turner/util/Generator.java
@@ -0,0 +1,140 @@
+/**
+ * 
+ */
+package jp.gr.java_conf.turner.util;
+
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * パイソン風ジェネレータクラス
+ * 
+ * @author TURNER
+ */
+public abstract class Generator<E> implements Iterable<E> {
+
+	public static void setExecutor(Executor exec) {
+		threadPool = exec;
+	}
+	
+	/**
+	 * 
+	 */
+	public Generator() {
+		this(0);
+	}
+
+	public Generator(int bufsize) {
+		switch (bufsize) {
+		case 0:
+			que = new SynchronousQueue<Maybe>();
+			break;
+		default:
+			que = new LinkedBlockingQueue<Maybe>(bufsize);
+		}
+	}
+
+	public abstract void run() throws InterruptedException;
+
+
+	protected void yield(E e) throws InterruptedException {
+		que.put(new Maybe(e));
+	}
+
+	private void execute() {
+		threadPool.execute(new Runnable() {
+
+			public void run() {
+				try {
+					try {
+						Generator.this.run();
+					} finally {
+						que.put(END_OF_QUE);
+					}
+				} catch (InterruptedException e) {
+					// NOP
+				}
+			}
+		});
+	}
+
+
+
+	private class Maybe {
+		private final E e;
+
+		Maybe(E e) {
+			this.e = e;
+		}
+
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Iterable#iterator()
+	 */
+	public Iterator<E> iterator() {
+
+		execute();
+
+		return new Iterator<E>() {
+			public boolean hasNext() {
+				Maybe m = END_OF_QUE;
+				try {
+					m = waitNext();
+				} catch (InterruptedException e) {
+					setEnd();
+				}
+				return m != END_OF_QUE;
+			}
+
+			public E next() {
+				Maybe m = END_OF_QUE;
+				try {
+					m = takeNext();
+				} catch (InterruptedException e) {
+					setEnd();
+				}
+				return m.e;
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+
+			private Maybe nextOne = null;
+
+			private void setEnd() {
+				nextOne = END_OF_QUE;
+			}
+
+			private Maybe waitNext() throws InterruptedException {
+				if (nextOne == null) {
+					nextOne = que.take();
+				}
+				return nextOne;
+			}
+
+			private Maybe takeNext() throws InterruptedException {
+				Maybe m = waitNext();
+				if (nextOne != END_OF_QUE) {
+					nextOne = null;
+				}
+				return m;
+			}
+		};
+	}
+
+	private static Executor threadPool = new Executor() {
+		public void execute(Runnable command) {
+			(new Thread(command)).start();
+		}
+	};
+
+	final private Maybe END_OF_QUE = new Maybe(null);
+
+	final private BlockingQueue<Maybe> que;
+}
---
+++ Generator/src/jp/gr/java_conf/turner/util/Sample.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package jp.gr.java_conf.turner.util;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author TURNER
+ * 
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		Generator<String> g = new Generator<String>() {
+
+			@Override
+			public void run() throws InterruptedException {
+				yield("‚Ù‚°");
+				yield("‚Ó‚ª");
+				yield("‚à‚°");
+			}
+		};
+
+		for (String s : g) {
+			System.out.println(s);
+		}
+
+		MyGenerator t2 = new MyGenerator(new File(args[0]));
+
+
+		for (File f : t2) {
+			System.out.println(f.getCanonicalPath());
+		}
+	}
+
+}
+
+class MyGenerator extends Generator<File> {
+	/**
+	 * 
+	 */
+	private File root;
+
+	public MyGenerator(File root) {
+		super(Integer.MAX_VALUE);
+		this.root = root;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see jp.gr.java_conf.turner.util.Generator.Task#run()
+	 */
+	@Override
+	public void run() throws InterruptedException {
+		listPath(root);
+	}
+
+	private void listPath(File dir) throws InterruptedException {
+		File[] fs = dir.listFiles();
+		for (File f : fs) {
+			if (f.isDirectory()) {
+				listPath(f);
+			} else if (f.isFile()) {
+				yield(f);
+			}
+		}
+	}
+
+}
